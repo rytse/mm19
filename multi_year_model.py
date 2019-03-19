b7f9f35c61e4a34b5c641634b0230581c9a824d5
Ryan Tse <rytse@protonmail.com>
2019-03-19 19:48:10 -0400

    Do everythign in pandas df (faster)

--- multi_year_model.py
+++ multi_year_model.py
@@ -2,103 +2,121 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm, zscore
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, WhiteKernel
+from sklearn.gaussian_process.kernels import *
+from sklearn.metrics.pairwise import sigmoid_kernel
 from sklearn.metrics import log_loss
 
 np.random.seed(1)
 
-V2U = ['Adj Off Efficiency', 'Adj Def Efficiency', 'Turnovers per game', 'Wins Last 10 Games',
-		'Points Allowed Per Game']
+tmp='''
+V2U = ['Adj Off Efficiency',
+		'Adj Off-Def',
+		'Adj Def Efficiency',
+		'FG%',
+		'Strength of Schedule',
+		'Avg. Scoring margin',
+		'Rebounds',
+		'Wins Last 10 Games',
+		'Turnovers per game',
+		'Coach Record']#'''
 
-for val_year in range(2014, 2018 + 1):
-	print(val_year)
-	train_years = list(range(2014, 2018+1))
-	train_years.remove(val_year)
-	train_years = [f'{train_year - 1}-{train_year}' for train_year in train_years]
-	val_year = f'{val_year - 1}-{val_year}'
+#V2U = ['Adj Off Efficiency', 'Adj Def Efficiency', 'Turnovers per game', 'Wins Last 10 Games',
+#		'Points Allowed Per Game']
 
-	# Load data
-	train_games = []
-	for train_year in train_years:
-		rep = pd.read_csv(f'{train_year}_combo.csv')
-		rep.rename(columns=lambda x: ' '.join(x.split()), inplace=True)
-		rep['Winner'] = rep['Winner'] + '_' + train_year
-		rep['Loser'] = rep['Loser'] + '_' + train_year
+V2U = ['Adj Off Efficiency', 'Adj Def Efficiency', 'SRS']
 
-		rep[rep.select_dtypes(include=[np.number]).columns].apply(zscore)
-		train_games.append(rep)
-	train_games = pd.concat(train_games)
+obest = 11.789993587770596
+rbest = 93.72828049656303
+wbest = 25.771604944753086
 
-	val_games = pd.read_csv(f'{val_year}_combo.csv')
-	val_games.rename(columns=lambda x: ' '.join(x.split()), inplace=True)
-	val_games[val_games.select_dtypes(include=[np.number]).columns].apply(zscore)
+sig1 = 3.2
+sig2 = 3.2
+ls1 = 93
+ls2 = 93
+sigw = 6
 
-	# Z-Score-ify data
+def flip_row(rep):
+	flip = np.random.random() > 0.5
 
-	# Construct list of features to use
-	winner_vars = []
-	loser_vars = []
-	for v in V2U:
-		winner_vars.append(v + ' Winner')
-		loser_vars.append(v + ' Loser')
+	if flip:
+		copy = rep.copy()
+		for name in rep.index:
+			if name.endswith('Loser'):
+				rep[name] = copy[name[0:-5] + 'Winner']
+			elif name.endswith('Winner'):
+				rep[name] = copy[name[0:-6] + 'Loser']
 
-	# Load each training game into a np array that will be the
-	# training data set
-	Xs = []  # input vars
-	ys = []  # output var
-	for idx, row in train_games.iterrows():
-		w_v = np.array(row[winner_vars])
-		l_v = np.array(row[loser_vars])
-		Xs.append(np.array([w_v, l_v]).flatten())
-		ys.append(np.array([row['Winner Points'] - row['Loser points']]))
-		Xs.append(np.array([l_v, w_v]).flatten())
-		ys.append(np.array([row['Loser points'] - row['Winner Points']]))
-	X = np.array(Xs)
-	y = np.array(ys)
+		rep['Loser'] = rep['Loser'] + '_' + year
+		rep['Winner'] = rep['Winner'] + '_' + year
+		rep['pdiff'] = -(rep['Winner Points'] - rep['Loser points'])
 
-	# Train a Gaussian Process Regression (GPR) model
-	obest = 11.789993587770596
-	rbest = 93.72828049656303
-	wbest = 25.771604944753086
-	kernel = obest * RBF(rbest) + WhiteKernel(wbest)
+	else:
+		rep['Winner'] = rep['Winner'] + '_' + year
+		rep['Loser'] = rep['Loser'] + '_' + year
+		rep['pdiff'] = rep['Winner Points'] - rep['Loser points']
+
+# Load all the game data
+game_data = []
+for year in range(2014, 2018+1):
+	year = f'{year - 1}-{year}'
+	rep = pd.read_csv(f'{year}_combo.csv')
+	rep.rename(columns=lambda x: ' '.join(x.split()), inplace=True)
+	rep[rep.select_dtypes(include=[np.number]).columns].apply(zscore)
+	rep['pdiff'] = rep['Winner Points'] - rep['Loser points']
+	rep['Winner'] = rep['Winner'] + '_' + year
+	rep['Loser'] = rep['Loser'] + '_' + year
+	game_data.append(rep)
+game_data = pd.concat(game_data)
+
+raw_names = [foo.strip() for foo in list(pd.read_csv('2014-2015_data.csv').columns)]
+raw_names.remove('Team Name')
+
+for i, row in game_data.iterrows():
+	if np.random.random() > 0.5:
+		for name in raw_names:
+			vname_w = name + ' Winner'
+			vname_l = name + ' Loser'
+			wv = game_data.at[i, vname_w]
+			game_data.at[i, vname_w] = game_data.at[i, vname_l]
+			game_data.at[i, vname_l] = wv
+		game_data.at[i, 'pdiff'] *= -1
+
+v2u_mod = []
+for v in V2U:
+	v2u_mod.append(v + ' Winner')
+	v2u_mod.append(v + ' Loser')
+
+def go():
+	# Set up Gaussian Process with its kernel (don't train yet)
+	rbf1 = sig1 ** 2 * RBF(length_scale=ls1)
+	rbf2 = sig2 ** 2 * RBF(length_scale=ls2)
+	white = sigw ** 2 * WhiteKernel(noise_level=sigw**2)
+	kernel = rbf1 + rbf2 + white
+
 	gp = GaussianProcessRegressor(kernel,
-		n_restarts_optimizer=100,
 		normalize_y=True)
-	gp.fit(X, y)
 
-	# Validate GPR
-	gp_y_pred = []
-	gp_sd = []
-	tys = []
-	for idx, row in val_games.iterrows():
-		w_v = np.array(row[winner_vars])
-		l_v = np.array(row[loser_vars])
+	for val_year in range(2014, 2018+1):
+		val_year = f'{val_year - 1}-{val_year}'
+		train = game_data[game_data['Winner'].str.endswith(val_year) == False]
+		val = game_data[game_data['Winner'].str.endswith(val_year) == True]
 
-		gp_y_pred_, gp_sd_ = gp.predict(np.array([w_v, l_v]).flatten().reshape(1, -1),
-										return_std=True)
-		gp_y_pred.append(gp_y_pred_)
-		gp_sd.append(gp_sd_)
-		tys.append(row['Winner Points'] - row['Loser points'])
+		# Select the proper subsets of the dataframe
+		X_train = train[v2u_mod]
+		y_train = train['pdiff']
+		X_val = val[v2u_mod]
+		y_val = val['pdiff']
 
-	gp_y_pred = np.array(gp_y_pred).flatten()
-	gp_sd = np.array(gp_sd).flatten()
-	tys = np.array(tys).flatten()
+		gp.fit(X_train, y_train)	# train
+		gp_y_pred, gp_sd = gp.predict(X_val, return_std=True)	# validate
 
-	# Calculate percent of games predicted correctly
-	correct = 0
-	for i in range(len(tys)):
-		if tys[i] >= 0:
-			if gp_y_pred[i] >= 0:
-				correct += 1
-		else:
-			if gp_y_pred[i] < 0:
-				correct += 1
 
-	print(f'{correct / len(tys) * 100}% of games predicted correctly')
+		# Calculate validation log loss
+		y_pred_probs = 1 - norm.cdf(-gp_y_pred / gp_sd)
+		ll = log_loss(np.array(y_val > 0, dtype=np.int),
+						np.array([1 - y_pred_probs, y_pred_probs]).T,
+						labels=[0, 1])
+		print(f'Log Loss: {ll}\n\n')
 
-	# Calculate validation log loss
-	y_pred_probs = 1 - norm.cdf(-gp_y_pred / gp_sd)
-	ll = log_loss(np.array(tys > 0, dtype=np.int),
-					np.array([1 - y_pred_probs, y_pred_probs]).T,
-					labels=[0, 1])
-	print(f'Log Loss: {ll}\n\n')
+
+go()
