b48bab7bdb7d3a99024faefc0d72a97c30dc57c5
Daniel Schaffer <threeschaffers@gmail.com>
2019-03-20 16:34:48 -0400

    Bugfix

--- fix_output.py
+++ fix_output.py
@@ -1,13 +1,20 @@
-inFile = open("2019_avg_out.csv", "r")
-outFile = open("2019_avg_out_fix.csv", "w")
+import sys
+
+inFile = open("2019_noisy_out.csv", "r")
+outFile = open("2019_noisy_out_fix.csv", "w")
 outFile.write("ID,Pred\n")
+count = 0
 for l in inFile:
     line = l.strip()
-    tokens = l.split(",")
+    tokens = line.split(",")
     tokens2 = tokens[0].split("_")
     team1 = tokens2[1]
     team2 = tokens2[2]
     if int(team1) > int(team2):
         outFile.write(tokens2[0] + "_" + team2 + "_" + team1 + "," + str(1-float(tokens[1])) + "\n")
+        count += 1
     else:
         outFile.write(line + "\n")
+        count += 1
+print(count, file=sys.stderr)
+outFile.close()
